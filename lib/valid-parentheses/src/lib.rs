094f1ebf1eb9bc532e585ea57f7fd2ad8d8ea271
Coutlaw <cass.d.outlaw@gmail.com>
2020-06-24 06:41:50 -0400

    Solution to valid parentheses

--- lib/valid-parentheses/src/lib.rs
+++ lib/valid-parentheses/src/lib.rs
@@ -35,15 +35,58 @@ Output: true
 
 
 */
-
 pub fn is_valid(s: String) -> bool {
-    true
+    use std::collections::HashMap;
+
+    // match closing parentheses to their corresponding opener
+    let mut completes = HashMap::new();
+    completes.insert(')', '(');
+    completes.insert('}', '{');
+    completes.insert(']', '[');
+
+    // use a stack and push values on as the occur, pop values as they are closed
+    let mut stack: Vec<char> = Vec::new();
+    // create Vec<char> from the argument, track previous value
+    let values: Vec<char> = s.trim().chars().collect();
+    let mut prev: char = ' ';
+
+    for value in values {
+        // is the value a closing paren, and does it close the most recent value
+        if let Some(v) = completes.get(&value) {
+            if v == &prev {
+                let _ = stack.pop().unwrap();
+                prev = match stack.len() {
+                    0 => ' ',
+                    _ => stack[stack.len() - 1],
+                };
+                continue;
+            }
+        }
+
+        // otherwise push it on the stack
+        prev = value;
+        stack.push(value);
+    }
+
+    stack.len() == 0
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(is_valid(String::from("{[()]}")), true);
+    }
+
+    #[test]
+    fn it_works2() {
+        assert_eq!(is_valid(String::from("{[}")), false);
+    }
+
+    #[test]
+    fn it_works3() {
+        assert_eq!(is_valid(String::from("{}[]()")), true);
     }
 }
